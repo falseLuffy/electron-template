dd9bd7706f333214b28579ea238961da3ec3b783
falseLuffy <falseLuffy@qq.com>
2019-11-11 18:07:21 +0800

    新增托盘功能

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu, Tray } from 'electron'
+import path from 'path'
 
 /**
  * Set `__static` path to static files in production
@@ -9,6 +10,10 @@ import { app, BrowserWindow } from 'electron'
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
+
+// 托盘对象
+var appTray = null
+let trayIcon = null
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
@@ -21,18 +26,79 @@ function createWindow () {
    */
   mainWindow = new BrowserWindow({
     height: 1080,
-    useContentSize: true,
     width: 1920,
+    useContentSize: true,
+    resizable: true,
     webPreferences: {
       webSecurity: false,
       nodeIntegration: true
     }
   })
 
+  let trayMenuTemplate = [
+    {
+      label: '设置',
+      click: function () {} // 打开相应页面
+    },
+    {
+      label: '意见反馈',
+      click: function () {}
+    },
+    {
+      label: '帮助',
+      click: function () {}
+    },
+    {
+      label: '关于demo',
+      click: function () {}
+    },
+    {
+      label: '退出demo',
+      click: function () {
+        // ipc.send('close-main-window');
+        app.quit()
+        appTray.destroy()
+        // mainWindow = null
+      }
+    }
+  ]
+
+  // 系统托盘图标目录
+  trayIcon = path.join(__dirname, '../../build/icons')
+
+  appTray = new Tray(path.join(trayIcon, 'icon.ico'))
+
+  // 图标的上下文菜单
+  const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
+
+  // 设置此托盘图标的悬停提示内容
+  appTray.setToolTip('This is my application.')
+
+  // 设置此图标的上下文菜单
+  appTray.setContextMenu(contextMenu)
+
+  appTray.setToolTip('My托盘测试')
+  appTray.setContextMenu(contextMenu)
+  appTray.on('double-click', () => { // 我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true)
+  })
+
   mainWindow.loadURL(winURL)
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
+  mainWindow.on('close', (event) => {
+    debugger
+    console.log(event)
+    mainWindow.hide()
+    mainWindow.setSkipTaskbar(true)
+    event.preventDefault()
+  })
+
+  mainWindow.on('show', () => {
+    appTray.setHighlightMode('always')
+  })
+  mainWindow.on('hide', () => {
+    appTray.setHighlightMode('never')
   })
 }
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'normalize.css'
+// import 'normalize.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
