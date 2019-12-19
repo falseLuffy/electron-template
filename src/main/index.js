a36089bce7655fe1051e503cde13080e50e08d86
falseLuffy <falseLuffy@qq.com>
2019-12-19 16:02:32 +0800

    代码格式化

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-import { app, BrowserWindow, Menu, Tray } from 'electron'
+// import {app, BrowserWindow, Menu, Tray} from 'electron'
+import {app, BrowserWindow, Menu} from 'electron'
 import path from 'path'
 // 隐藏菜单
 Menu.setApplicationMenu(null)
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV !== 'development') {
 
 // 托盘对象
 var appTray = null
-let trayIcon = null
+// let trayIcon = null
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
@@ -24,8 +25,8 @@ const winURL = process.env.NODE_ENV === 'development'
 
 function createWindow () {
   /**
-   * Initial window options
-   */
+    * Initial window options
+    */
   mainWindow = new BrowserWindow({
     height: 1200,
     width: 1920,
@@ -38,60 +39,59 @@ function createWindow () {
     frame: false // 去除边框及标题栏
   })
 
-  // 工具托盘的菜单
-  let trayMenuTemplate = [
-    {
-      label: '设置',
-      click: function () {} // 打开相应页面
-    },
-    {
-      label: '意见反馈',
-      click: function () {}
-    },
-    {
-      label: '帮助',
-      click: function () {}
-    },
-    {
-      label: '关于demo',
-      click: function () {}
-    },
-    {
-      label: '退出demo',
-      click: function () {
-        // ipc.send('close-main-window');
-        app.quit()
-        appTray.destroy()
-        // mainWindow = null
+  /* // 工具托盘的菜单
+    let trayMenuTemplate = [
+      {
+        label: '设置',
+        click: function () {} // 打开相应页面
+      },
+      {
+        label: '意见反馈',
+        click: function () {}
+      },
+      {
+        label: '帮助',
+        click: function () {}
+      },
+      {
+        label: '关于demo',
+        click: function () {}
+      },
+      {
+        label: '退出demo',
+        click: function () {
+          // ipc.send('close-main-window');
+          app.quit()
+          appTray.destroy()
+          // mainWindow = null
+        }
       }
-    }
-  ]
+    ]
 
-  // 系统托盘图标目录
-  trayIcon = path.join(__dirname, '../../build/icons')
+    // 系统托盘图标目录
+    trayIcon = path.join(__dirname, '../../build/icons')
 
-  appTray = new Tray(path.join(trayIcon, 'icon.ico'))
+    appTray = new Tray(path.join(trayIcon, 'icon.ico'))
 
-  // 图标的上下文菜单
-  const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
+    // 图标的上下文菜单
+    const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
 
-  // 设置此托盘图标的悬停提示内容
-  appTray.setToolTip('This is my application.')
+    // 设置此托盘图标的悬停提示内容
+    appTray.setToolTip('This is my application.')
 
-  // 设置此图标的上下文菜单
-  appTray.setContextMenu(contextMenu)
+    // 设置此图标的上下文菜单
+    appTray.setContextMenu(contextMenu)
 
-  appTray.setToolTip('My托盘测试')
-  appTray.setContextMenu(contextMenu)
-  appTray.on('double-click', () => { // 我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
+    appTray.setToolTip('My托盘测试')
+    appTray.setContextMenu(contextMenu) */
+  /* appTray.on('double-click', () => { // 我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
     mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
     mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true)
-  })
+  }) */
 
   mainWindow.loadURL(winURL)
 
   mainWindow.on('close', (event) => {
-    debugger
     console.log(event)
     mainWindow.hide()
     mainWindow.setSkipTaskbar(true)
